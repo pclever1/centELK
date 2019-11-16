f1e058dce0564309a48189771054521b4c0c08b0
Peter Cleverdon <3371338+pclever1@users.noreply.github.com>
2019-11-15 22:18:31 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,5 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-yum -y install net-tools
 myip=$(eval "ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'")
 echo $myip
