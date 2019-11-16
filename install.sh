d717673b7ada978ace0de19e816e4c34aac3e9c0
Peter Cleverdon <3371338+pclever1@users.noreply.github.com>
2019-11-15 21:57:43 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+yum install java
+yum install wget
+rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+cd /etc/yum.repos.d/
+sudo wget https://raw.githubusercontent.com/pclever1/centELK/master/elasticsearch.repo
+yum install elasticsearch && yum install kibana && yum install logstash
+cd /etc/logstash/conf.d
+sudo wget https://raw.githubusercontent.com/a3ilson/pfelk/master/conf.d/01-inputs.conf
+sudo wget https://raw.githubusercontent.com/a3ilson/pfelk/master/conf.d/05-syslog.conf
+sudo wget https://raw.githubusercontent.com/a3ilson/pfelk/master/conf.d/10-pf.conf
+sudo wget https://raw.githubusercontent.com/a3ilson/pfelk/master/conf.d/11-firewall.conf
+sudo wget https://raw.githubusercontent.com/a3ilson/pfelk/master/conf.d/50-outputs.conf
+sudo wget https://raw.githubusercontent.com/a3ilson/pfelk/master/conf.d/12-suricata.conf
+sudo wget https://raw.githubusercontent.com/a3ilson/pfelk/master/conf.d/13-snort.conf
+sudo wget https://raw.githubusercontent.com/a3ilson/pfelk/master/conf.d/15-others.conf
+mkdir /etc/logstash/conf.d/patterns
+/bin/systemctl daemon-reload
+/bin/systemctl enable elasticsearch.service
+/bin/systemctl enable kibana.service
+/bin/systemctl enable logstash.service
+systemctl stop firewalld
