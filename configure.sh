ea3fc17ed0bfed9aa5f2e25861fb6bf0042c4bb4
Peter Cleverdon <3371338+pclever1@users.noreply.github.com>
2019-11-15 22:51:34 -0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,3 +1,3 @@
 ip=$(eval "ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'")
 sed -i 's/#http.port: 9200/http.port: 9200/g' /etc/elasticsearch/elasticsearch.yml
-sed -i 's/#network.host: 192.168.0.1/network.host: '$ip'/g' /etc/elasticsearch/elasticsearch.yml
+sed -i 's/#network.host: 192.168.0.1/#network.host: 192.168.0.1/g' /etc/elasticsearch/elasticsearch.yml
