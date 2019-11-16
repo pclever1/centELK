68733ab86bacbca237b063fc442ffbd664aa6d12
Peter Cleverdon <3371338+pclever1@users.noreply.github.com>
2019-11-15 22:18:01 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 yum -y install net-tools
-alias myip=ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
-echo myip
+myip=$(eval "ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'")
+echo $myip
