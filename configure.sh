08aa4baded09306adc8a99b204364193b7f6a3a2
Peter Cleverdon <3371338+pclever1@users.noreply.github.com>
2019-11-15 23:08:45 -0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,3 +1,6 @@
 ip=$(eval "ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'")
-echo $ip
-sed -i "s/#discovery.seed_hosts: [\"host1\", \"host2\"]/#discovery.seed_hosts: [\"127.0.0.1\", \"$ip\"]/g" /etc/elasticsearch/elasticsearch.yml
+sed -i "s/#network.host: 192.168.0.1/network.host: $ip/g" /etc/elasticsearch/elasticsearch.yml
+sed -i 's/#http.port: 9200/http.port: 9200/g' /etc/elasticsearch/elasticsearch.yml
+echo '#discovery.seed_hosts: ["host1", "host2"]' | sed "s/#discovery.seed_hosts/discovery.seed_hosts/g"
+echo '#discovery.seed_hosts: ["host1", "host2"]' | sed "s/host1/127.0.0.1/g"
+echo '#discovery.seed_hosts: ["host1", "host2"]' | sed "s/host2/$ip/g"
