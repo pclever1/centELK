331c530a161d8c173bc3007f88c96d59e7a5ac1a
Peter Cleverdon <3371338+pclever1@users.noreply.github.com>
2019-11-15 23:38:32 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-echo "start"
-read -p "What is your pfSense IP: " pfip
+echo 'start'
+read -p 'What is your pfSense IP: ' pfip
 echo $pfip
-echo "end"
+echo 'end'
 pfip=`echo $pfip | sed 's/\./\\\\./g'`
 echo $pfip | sed 's/172\\.22\\.33\\.1/$pfip/g'
