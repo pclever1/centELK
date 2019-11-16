f4fe8d35448943fb0ec02e626e5b806562c8f1b8
Peter Cleverdon <3371338+pclever1@users.noreply.github.com>
2019-11-15 22:20:46 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,3 @@
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-myip=$(eval "ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'")
-echo $myip
-
-
 x="ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'"
 eval "$x"
 y=$(eval "$x")
