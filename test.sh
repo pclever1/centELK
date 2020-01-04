4cc95aa54a12b6ee8338a6cf5b08c5dcc3b7ecfe
Peter Cleverdon <3371338+pclever1@users.noreply.github.com>
2020-01-04 15:17:34 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
-read -p 'Username: ' uservar
-read -sp 'Password: ' passvar
-echo
-echo Thankyou $uservar we now have your login details
+#check if root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+#install ELK
+rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+cd /etc/yum.repos.d/
+sudo wget https://raw.githubusercontent.com/pclever1/centELK/master/elasticsearch.repo
+yum -y install elasticsearch
+yum -y install kibana
+yum -y install logstash
